18d15140018da95f27a864f13a1b447b571f00bd
krystian.bersztolc <krystian.bersztolc@agreeyamobility.net>
2014-05-16 20:17:14 +0200

    Changed GUI, prepare to add geofences

---
+++ ubudu-sdk-demo2/src/com/ubudu_sdk_demo2/BeaconFragment.java
@@ -0,0 +1,80 @@
+package com.ubudu_sdk_demo2;
+
+import com.ubudu.sdk.UbuduSDK;
+
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class BeaconFragment extends UbuduFragment implements OnClickListener{
+
+	private RelativeLayout mActiveSpot;
+	
+	public static BeaconFragment newInstance() {
+		return new BeaconFragment();
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.beacon_fragment, container, false);
+		
+		create((TextView) view.findViewById(R.id.startB), (TextView) view.findViewById(R.id.infoLabel), ((MainActivity) getActivity()).getOutput());
+		
+		mActiveSpot = (RelativeLayout) view.findViewById(R.id.activeSpot);
+		mActiveSpot.setOnClickListener(this);
+		
+		return view;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		activateBluetoothIfPossible(((MainActivity)getActivity()).getOutput());
+	}
+	
+	@Override
+	public int getMode() {
+		return BEACON;
+	}
+	
+	private boolean activateBluetoothIfPossible(TextOutput textOutput) {
+		BluetoothAdapter bt = BluetoothAdapter.getDefaultAdapter();
+		// Check Availability of bluetooth
+		if (bt == null) {
+			textOutput.printf("Bluetooth Not Available on this device.\n");
+			return false;
+		} else {
+			if (!bt.isEnabled()) {
+				bt.enable();
+				textOutput.printf("Bluetooth enabled.\n");
+			}
+			return true;
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.activeSpot:
+			{
+				if(!isScanningActive()) {
+					getTextOutput().printf("Start searching for beacons\n");
+					Error e = ((MainActivity)getActivity()).getBeaconManager().start(getActivity());
+					startScanning(e);
+				} else {
+					((MainActivity)getActivity()).getBeaconManager().stop(getActivity());
+					UbuduSDK.getSharedInstance(getActivity().getApplicationContext()).release(getActivity().getApplicationContext());
+					stopScanning();
+				}
+			}
+		}
+	}
+	
+	
+}
--- ubudu-sdk-demo2/src/com/ubudu_sdk_demo2/MainActivity.java
+++ ubudu-sdk-demo2/src/com/ubudu_sdk_demo2/MainActivity.java
@@ -1,66 +1,75 @@
 package com.ubudu_sdk_demo2;
 
 import com.ubudu.sdk.UbuduAreaDelegate;
-import com.ubudu.sdk.UbuduBeacon;
 import com.ubudu.sdk.UbuduBeaconManager;
+import com.ubudu.sdk.UbuduGeofenceManager;
 import com.ubudu.sdk.UbuduSDK;
 
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.text.Layout;
-import android.text.method.ScrollingMovementMethod;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements TextOutput {
+public class MainActivity extends FragmentActivity implements TextOutput {
+	
+	// put here your namespace
+	private static final String NAMESPACE = "ed2f594c2eb20f3a1213e387af53cd86fa1f70e0";
 
-	private TextView mInfoLabel, mOutputText;
-	private ProgressBar mProgressBar;
-	private Button mActionButton;
+	private UbuduPagerAdapter mUbuduPagerAdapter;
+	private ViewPager mViewPager;
 
-	private InfoAreaReceiver mInfoReceiver;
-
+	private UbuduSDK mUbuduSdk;
+	
 	private UbuduBeaconManager mBeaconManager;
-	private UbuduAreaDelegate mBeaconDelegate;
-
-	// Use your application namespace
-	private static final String namespace = "71b309965b54bdd8d3594a1253fb0da408154982";
-
-	private boolean isScanningActive = false;
-	private UbuduSDK sdk;
-
+	private InfoAreaReceiver mInfoAreaReceiver;
+	private UbuduAreaDelegate mAreaDelegate;
+	
+	private TextView mOutputText;
+	
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	protected void onCreate(Bundle arg0) {
+		super.onCreate(arg0);
 		setContentView(R.layout.activity_main);
-
-		System.out.println("onCreate()");
 		
-		mInfoLabel = (TextView) findViewById(R.id.informationLabel);
 		mOutputText = (TextView) findViewById(R.id.outputText);
-		mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
-		mActionButton = (Button) findViewById(R.id.actionButton);
-
-		mActionButton.setOnClickListener(actionButtonListener);
-		mOutputText.setMovementMethod(new ScrollingMovementMethod());
-
-		sdk = UbuduSDK.getSharedInstance(getApplicationContext());
-		sdk.setNamespace(namespace);
 		
-		sdk.setMaximumDailyNumberOfNotificationsAllowed(99);
-
-		mBeaconDelegate = new DemoAreaDelegate(this);
-		mBeaconManager = sdk.getBeaconManager();
-		mBeaconManager.setAreaDelegate(mBeaconDelegate);
-
-		activateBluetoothIfPossible();
+		mUbuduSdk = UbuduSDK.getSharedInstance(getApplicationContext());
+		mUbuduSdk.setNamespace(NAMESPACE);
+		mUbuduSdk.setMaximumDailyNumberOfNotificationsAllowed(9999);
+		
+		mAreaDelegate = new DemoAreaDelegate(this);
+		mBeaconManager = mUbuduSdk.getBeaconManager();
+		
+		mBeaconManager.setAreaDelegate(getAreaDelegate());
+		
+		mUbuduPagerAdapter = new UbuduPagerAdapter(getSupportFragmentManager());
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mUbuduPagerAdapter);
 	}
-
+	
+	public UbuduBeaconManager getBeaconManager() {
+		return mBeaconManager;
+	}
+	
+	public InfoAreaReceiver getInfoAreaReceiver() {
+		return mInfoAreaReceiver;
+	}
+	
+	public UbuduAreaDelegate getAreaDelegate() {
+		return mAreaDelegate;
+	}
+	
+	public UbuduSDK getUbuduSDK() {
+		return mUbuduSdk;
+	}
+	
+	public TextOutput getOutput() {
+		return this;
+	}
+	
 	@Override
 	public void printf(String formatControl, Object... arguments) {
 		final String newText = String.format(formatControl, arguments);
@@ -74,12 +83,12 @@ public class MainActivity extends Activity implements TextOutput {
 			}
 		}
 	}
-
+	
 	private void registerInfoReceiver() {
 		synchronized (this) {
-			if (mInfoReceiver == null) {
-				mInfoReceiver = new InfoAreaReceiver(this);
-				registerReceiver(mInfoReceiver, new IntentFilter("com.ubudu.sdk.notify"));
+			if (mInfoAreaReceiver == null) {
+				mInfoAreaReceiver = new InfoAreaReceiver(this);
+				registerReceiver(mInfoAreaReceiver, new IntentFilter("com.ubudu.sdk.notify"));
 				this.printf("Registered info receiver.\n");
 			}
 		}
@@ -87,98 +96,19 @@ public class MainActivity extends Activity implements TextOutput {
 
 	private void unregisterInfoReceiver() {
 		synchronized (this) {
-			if (mInfoReceiver != null) {
-				unregisterReceiver(mInfoReceiver);
-				mInfoReceiver = null;
+			if (mInfoAreaReceiver != null) {
+				unregisterReceiver(mInfoAreaReceiver);
+				mInfoAreaReceiver = null;
 				this.printf("Unregistered info receiver.\n");
 			}
 		}
 	}
-
-	private boolean activateBluetoothIfPossible() {
-		BluetoothAdapter bt = BluetoothAdapter.getDefaultAdapter();
-		// Check Availability of bluetooth
-		if (bt == null) {
-			this.printf("Bluetooth Not Available on this device.\n");
-			return false;
-		} else {
-			if (!bt.isEnabled()) {
-				bt.enable();
-			}
-			return true;
-		}
-	}
-
-	private void startSearchingBeacons() {
-		this.printf("Start searching for beacons");
-		Error e = mBeaconManager.start(this);
-		if (e != null) {
-			this.printf("Error: %s\n", e.toString());
-			isScanningActive = false;
-			refreshActionButtonState();
-		} else {
-			isScanningActive = true;
-			refreshActionButtonState();
-		}
-	}
-
-	private void stopSearchingBeacons() {
-		this.printf("Stop searching for beacons");
-		mBeaconManager.stop(this);
-		
-		UbuduSDK.getSharedInstance(getApplicationContext()).release(getApplicationContext());
-		
-		isScanningActive = false;
-		refreshActionButtonState();
-	}
-
-	private void refreshActionButtonState() {
-		if (!isScanningActive) {
-			mInfoLabel.setText("Press button to start searching for beacons");
-			mActionButton.setText("START");
-		} else {
-			mInfoLabel.setText("Press button to stop searching for beacons");
-			mActionButton.setText("STOP");
-		}
-	}
 	
-	public void onConfigurationChanged(android.content.res.Configuration newConfig) {
-		if (isScanningActive) {
-			stopSearchingBeacons();
-			startSearchingBeacons();
-			mInfoLabel.setText("After cofiguration changed");
-		}
-	};
-	
-	public void onContentChanged() {
-		if (isScanningActive) {
-			stopSearchingBeacons();
-			startSearchingBeacons();
-			mInfoLabel.setText("After content changed");
-		}
-	};
-	
-	OnClickListener actionButtonListener = new OnClickListener() {
-
-		@Override
-		public void onClick(View v) {
-			if (!isScanningActive) {
-				startSearchingBeacons();
-			} else {
-				stopSearchingBeacons();
-			}
-		}
-	};
-
 	@Override
 	protected void onResume() {
 		super.onResume();
 		registerInfoReceiver();
 		
-		if (sdk.getBeaconManager().isMonitoring()) {
-			isScanningActive = true;
-			refreshActionButtonState();
-		}
 	}
 	
 	@Override
@@ -186,5 +116,6 @@ public class MainActivity extends Activity implements TextOutput {
 		super.onPause();
 		unregisterInfoReceiver();
 	}
-
+	
+	
 }
