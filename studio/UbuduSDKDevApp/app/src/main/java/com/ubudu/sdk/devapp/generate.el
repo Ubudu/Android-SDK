9abf87ba11a92e71602d25c6469153e1862bd9c2
Pascal J. Bourguignon <pjb@informatimago.com>
2014-08-27 14:53:23 +0200

    Added headers.

--- studio/UbuduSDKDevApp/app/src/main/java/com/ubudu/sdk/devapp/generate.el
+++ studio/UbuduSDKDevApp/app/src/main/java/com/ubudu/sdk/devapp/generate.el
@@ -1,13 +1,65 @@
-;;;; -*- mode:emacs-lisp; coding:utf-8 -*-
-;;;;
-;;;; This file generates the dto (Data Transfer Object) classes for
-;;;; the Ubudu API JSON structures.
-;;;;
-;;;;
+;;;; -*- mode:emacs-lisp;coding:utf-8 -*-
+;;;;**************************************************************************
+;;;;FILE:               generate.el
+;;;;LANGUAGE:           emacs lisp
+;;;;SYSTEM:             POSIX
+;;;;USER-INTERFACE:     NONE
+;;;;DESCRIPTION
+;;;;    
+;;;;    This file generates the dto (Data Transfer Object) classes for
+;;;;    the Ubudu API JSON structures.
+;;;;    
+;;;;AUTHORS
+;;;;    <PJB> Pascal J. Bourguignon <pjb@informatimago.com>
+;;;;MODIFICATIONS
+;;;;    2014-08-27 <PJB> Added this header.
+;;;;BUGS
+;;;;LEGAL
+;;;;    ubudu-public
+;;;;    
+;;;;    Copyright (c) 2011-2014, UBUDU SAS
+;;;;    All rights reserved.
+;;;;    
+;;;;    Redistribution and use in source and binary forms, with or without
+;;;;    modification, are permitted provided that the following conditions are met:
+;;;;    
+;;;;    * Redistributions of source code must retain the above copyright notice, this
+;;;;      list of conditions and the following disclaimer.
+;;;;    
+;;;;    * Redistributions in binary form must reproduce the above copyright notice,
+;;;;      this list of conditions and the following disclaimer in the documentation
+;;;;      and/or other materials provided with the distribution.
+;;;;    
+;;;;    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;;;;    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;;;;    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;;;;    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+;;;;    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;;;;    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;;;;    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+;;;;    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+;;;;    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+;;;;    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;;;;**************************************************************************
 
+;;;;---------------------------------------------------------------------
+;;; Skip to the Entities section to add/edit the dto entities.
+;;;---------------------------------------------------------------------
+(setq lexical-binding t)
+(setq-default indent-tabs-mode nil)
+(require 'cl)
+;;;---------------------------------------------------------------------
 
-(defvar *dirpath* "~/firms/ubudu/src/Android-SDK/ubudu-sdk-dev-app/src/com/ubudu/sdk/devapp/")
+(defmacro defparameter (symbol &optional initvalue docstring)
+  `(progn
+     (defvar ,symbol nil ,docstring)
+     (setq   ,symbol ,initvalue)))
+
+(defparameter *dirpath* default-directory "Where the java files will be generated.")
 (defparameter *java-current-package* 'com.ubudu.sdk.devapp)
+(message "dirpath= %S" *dirpath*)
+
+;;;---------------------------------------------------------------------
 
 (defparameter *java-operators* '(+ - * / < > <= >= == ! && ||))
 
